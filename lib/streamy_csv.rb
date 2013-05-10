aeb669ec171ac2424a505de3e8193f635b243037
smsohan <sohan39@gmail.com>
2013-05-09 19:48:01 -0600

    added the streaming logic

---
+++ lib/streamy_csv.rb
@@ -0,0 +1,45 @@
+require "streamy_csv/version"
+
+module StreamyCsv
+
+  # stream_csv('data.csv', MyModel.header_row) do |rows|
+  #   MyModel.find_each do |my_model|
+  #     rows << my_model.to_csv_row
+  #   end
+  # end
+  #
+  #
+
+  def stream_csv(file_name, header_row, &block)
+    set_streaming_headers
+    set_file_headers(file_name)
+
+    response.status = 200
+    self.response_body = csv_lines(header_row, &block)
+  end
+
+  protected
+
+  def set_streaming_headers
+    headers['X-Accel-Buffering'] = 'no'
+    headers["Cache-Control"] ||= "no-cache"
+    headers.delete("Content-Length")
+  end
+
+  def csv_lines(header_row, &block)
+
+    Enumerator.new do |rows|
+      rows << header_row.to_s if header_row
+      block.call(rows)
+    end
+
+  end
+
+  def set_file_headers(file_name)
+    headers["Content-Type"] = "text/csv"
+    headers["Content-disposition"] = "attachment; filename=\"#{file_name}\""
+  end
+
+end
+
+ActionController::Base.send :include, StreamyCsv
---
+++ streamy_csv.gemspec
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'streamy_csv/version'
+
+Gem::Specification.new do |gem|
+  gem.name          = "streamy_csv"
+  gem.version       = StreamyCsv::VERSION
+  gem.authors       = ["smsohan"]
+  gem.email         = ["sohan39@gmail.com"]
+  gem.description   = %q{Streamy CSV lets you stream live generated CSV files}
+  gem.summary       = %q{Provides a simple API for your controllers to stream CSV files one row at a time}
+  gem.homepage      = ""
+
+  gem.files         = `git ls-files`.split($/)
+  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ["lib"]
+end
